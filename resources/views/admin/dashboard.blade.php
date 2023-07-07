406dcf0130e442f03acaaa333e1c0caeff71e645
rigo mac <mbandrigo@gmail.com>
2023-07-07 10:04:02 +0800

    upload

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -48,58 +48,10 @@
             })
         }
         async function uploadSoal() {
-            let dataSend = new FormData()
-            dataSend.append('filter', "20233")
-            let sendRequest = await fetch("https://sia.iainkendari.ac.id/konseling_api/eksport_data_dari_sia", {
-                method: "POST",
-                body: dataSend
-            })
-            let response = await sendRequest.json()
-            console.log(response);
-            // return
-            response.data.map(async (item, index) => {
-                // if (index == 0) {
-                // console.log(item);
-                // return console.log('ggwp');
-                let url = "{{route('sinkron')}}";
-                let dataSend2 = new FormData()
-                dataSend2.append('data', JSON.stringify(item))
-                let sendRequest2 = await fetch(url, {
-                    method: "POST",
-                    body: dataSend2
-                })
-                let response2 = await sendRequest2.json()
-                console.log(response2);
-                // }
-                // console.log(item.nama);
-            })
+
         }
         async function buatAkunPengawas() {
-            let dataSend = new FormData()
-            dataSend.append('filter', "20233")
-            let sendRequest = await fetch("https://sia.iainkendari.ac.id/konseling_api/eksport_data_dari_sia", {
-                method: "POST",
-                body: dataSend
-            })
-            let response = await sendRequest.json()
-            console.log(response);
-            // return
-            response.data.map(async (item, index) => {
-                // if (index == 0) {
-                // console.log(item);
-                // return console.log('ggwp');
-                let url = "{{route('sinkron')}}";
-                let dataSend2 = new FormData()
-                dataSend2.append('data', JSON.stringify(item))
-                let sendRequest2 = await fetch(url, {
-                    method: "POST",
-                    body: dataSend2
-                })
-                let response2 = await sendRequest2.json()
-                console.log(response2);
-                // }
-                // console.log(item.nama);
-            })
+
         }
     </script>
 </body>
