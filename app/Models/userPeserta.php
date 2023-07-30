0b0c13dbfe2f60147e81aa3b78cd35824fdf1f4c
rigo mac <mbandrigo@gmail.com>
2023-07-30 22:45:35 +0800

    tahap 2 mandiri

--- app/Models/userPeserta.php
+++ app/Models/userPeserta.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userPeserta extends Model
+class UserPeserta extends Model
 {
     use HasFactory;
 
