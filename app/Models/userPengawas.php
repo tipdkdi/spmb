ea6b96d1d2a092fe592173666d80bdf7ea912096
rigo mac <mbandrigo@gmail.com>
2023-07-30 22:44:21 +0800

    tahap 2 mandiri

--- app/Models/userPengawas.php
+++ app/Models/userPengawas.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userPengawas extends Model
+class UserPengawas extends Model
 {
     use HasFactory;
 
