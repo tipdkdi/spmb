db5ae18a3744d190970bf8076bfd5ac4ca7066b0
rigo mac <mbandrigo@gmail.com>
2023-07-30 23:29:30 +0800

    tahap 2 mandiri

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        URL::forceScheme('https');
+
         Schema::defaultStringLength(191);
     }
 }
