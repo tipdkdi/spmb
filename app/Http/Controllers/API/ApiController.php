bad8b9414cce28c22c5702c5ccb378400e07dd08
rigo mac <mbandrigo@gmail.com>
2024-07-14 16:43:05 +0800

    uml2024

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -496,7 +496,7 @@ class ApiController extends Controller
     }
     public function storeSoal(Request $request)
     {
-        return $request->opsi;
+        // return $request->opsi;
         DB::beginTransaction();
 
         try {
