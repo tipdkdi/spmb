38cea5de8a6dc078b6487a4b499a1b69f818c1ed
rigo mac <mbandrigo@gmail.com>
2024-07-14 16:57:49 +0800

    uml2024

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -496,7 +496,7 @@ class ApiController extends Controller
     }
     public function storeSoal(Request $request)
     {
-        return $request->opsi[0]->is_jawaban;
+        return $request->opsi;
         DB::beginTransaction();
 
         try {
@@ -507,6 +507,13 @@ class ApiController extends Controller
                 'soal' => $request->soal,
             ]);
             if ($request->soal_id == null) {
+                for ($i = 0; $i <= 3; $i++) {
+                    SoalOpsi::create([
+                        'soal_id' => $soal->id,
+                        'opsi_text' => $item['opsi_text'],
+                        'is_jawaban' => $item['is_jawaban'],
+                    ]);
+                }
                 foreach ($request->opsi as $item) {
                     SoalOpsi::create([
                         'soal_id' => $soal->id,
